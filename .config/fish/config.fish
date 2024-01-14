365a4314071e958e82d76dd7cc0a385c4da6a390
Horváth Norbert <nnorbert204@gmail.com>
2024-01-14 14:27:40 +0100

    Updated really old fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,10 +3,21 @@ if status is-interactive
 end
 
 function fish_greeting
-	sh ~/fetch.sh
+    sh ~/fetch.sh
 end
 
-alias sysupdate="sudo aura -Syu && sudo aura -Au && flatpak update"
+# TODO: make this alias more universal
+alias sysupdate="sudo aura -S archlinux-keyring && sudo aura -Syu && sudo aura -Au && cargo install-update --all && sudo flatpak update"
 alias roll="curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash"
+alias py="python3"
+alias vim="nvim"
+alias ls="exa"
+alias dotnet_install="~/.dotnet/dotnet-install.sh"
+
+set -gx DOTNET_ROOT $HOME/.dotnet
+
+fish_add_path $DOTNET_ROOT
+fish_add_path "/var/lib/flatpak/exports/share"
+fish_add_path "~/.local/bin"
 
 starship init fish | source
